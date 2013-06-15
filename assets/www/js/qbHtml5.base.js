6bfe5a99de7900e0d2487443f6e672baca06dd12
wuji1626 <wuji1626@hotmail.com>
2013-06-15 19:42:59 +0800

    list's style mended

--- assets/www/js/qbHtml5.base.js
+++ assets/www/js/qbHtml5.base.js
@@ -4,9 +4,9 @@ var qbHtml5mobi = {
   version: '0.5.0',
   //website: 'http://quickbundle.org/'
   //根据实际的web容器端口确定
-  //website: 'http://10.0.0.2:9090/JAX/services/userService/'
+  website: 'http://10.0.2.2:9090/JAX/services/userService/'
   //浏览器调试地址
-  website: 'http://127.0.0.1:9090/JAX/services/userService/'
+  //website: 'http://127.0.0.1:9090/JAX/services/userService/'
 }
 qbHtml5mobi.utils = {
   setParam: function(name, value){
--- assets/www/js/qbHtml5.receipt.js
+++ assets/www/js/qbHtml5.receipt.js
@@ -49,16 +49,20 @@ function formatReceiptList(data){
 	//clear ul
 	$('ul').html('');
   	for(var i=0; i < data.length;i++){
-		var li = "<li class='lst' data-role='fieldcontain' data-icon='delete-msg'>" + 
-		"<a href='#' data-role='button' onclick='changeToReceiptEdit(" +data[i].receiptNum+ ")'>" +
-		"<font size=2>" + 
+		var li = "<li class='lst' data-role='list-dovider' data-icon='true'>" + 
+		"<a href='#' data-role='button' data-icon='delete' onclick='changeToReceiptEdit(" +data[i].receiptNum+ ")'>" +
+		"<font size=1>" + 
 		//申请人
-		qbHtml5mobi.constants.receiptApplicant + qbHtml5mobi.constants.colon + data[i].receiptApplicant + qbHtml5mobi.constants.space +
+		//qbHtml5mobi.constants.receiptApplicant + qbHtml5mobi.constants.colon + data[i].receiptApplicant + qbHtml5mobi.constants.space +
+		" 申请人：" + data[i].receiptApplicant + "<br/>" +
 		//单据
-		qbHtml5mobi.constants.receiptName + qbHtml5mobi.constants.colon + data[i].receiptTitle + qbHtml5mobi.constants.space +
+		//qbHtml5mobi.constants.receiptName + qbHtml5mobi.constants.colon + data[i].receiptTitle + qbHtml5mobi.constants.space +
+		" 单据：" +  data[i].receiptTitle + "<br/>" +
 		//单据日期
-		qbHtml5mobi.constants.receiptDate + qbHtml5mobi.constants.colon + data[i].receiptDate + "</font></a>" +
-		"<a href='#' data-role='button' data-icon='delete' onclick='deleteReceipt(" + data[i].receiptNum + ")'><img src='images/delete.png' /></a>" +
+		//qbHtml5mobi.constants.receiptDate + qbHtml5mobi.constants.colon + data[i].receiptDate + 
+		"单据日期：" + data[i].receiptDate + "<br/>" +
+		"</font></a>" +
+		"<a href='#' data-icon='delete' onclick='deleteReceipt(" + data[i].receiptNum + ")'><img src='images/delete.png' /></a>" +
 		"</li>";
 		$(li).appendTo("ul");
 	}
@@ -69,7 +73,6 @@ function changeToReceiptEdit(id){
 	window.location.href = "newreceipt.html";
 }
 function deleteReceipt(id){
-	if(window.confirm("是否删除？")){
 	$.ajax( {
 					async:false,
 					type:'GET',
@@ -86,5 +89,4 @@ function deleteReceipt(id){
 					    window.location.href = "index.html";
 					}
 				});	
-	}
 }
